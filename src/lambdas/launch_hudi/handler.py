13844a5b5bd7085a39a468ad63ea2695fddb85c6
Adam North <anorth848@gmail.com>
2022-09-29 15:40:50 -0400

    fix permissions

--- src/lambdas/launch_hudi/handler.py
+++ src/lambdas/launch_hudi/handler.py
@@ -67,7 +67,7 @@ def get_hudi_configs(identifier, table_name, table_config, pipeline_type):
     record_key = table_config['record_key']
     precombine_field = table_config['source_ordering_field']
 
-    source_s3uri = os.path.join(raw_lake_uri, identifier, table_name.replace('_', '/', 2), '')
+    source_s3uri = os.path.join(raw_lake_uri, identifier, table_name.replace('_', '/', 3), '')
 
     hudi_conf = {
         'hoodie.clustering.inline': 'true',
@@ -129,7 +129,7 @@ def generate_steps(identifier, configs, pipeline_type):
         config = configs['StepConfigs'][table]
         logger.debug(json.dumps(config, indent=4))
         if 'enabled' in config and config['enabled'] is True:
-            table_name = table.replace('.', '_')
+            table_name = f'{identifier}_{table.replace(".", "_")}'
 
             if 'spark_conf' in config and pipeline_type in config['spark_conf']:
                 for k, v in config['spark_conf'][pipeline_type].items():
@@ -141,7 +141,7 @@ def generate_steps(identifier, configs, pipeline_type):
                 '--source-class', 'org.apache.hudi.utilities.sources.ParquetDFSSource',
                 '--enable-sync',
                 '--target-table', table_name,
-                '--target-base-path', os.path.join(curated_lake_uri, identifier, table_name, ''),
+                '--target-base-path', os.path.join(curated_lake_uri, glue_database, table_name, ''),
                 '--source-ordering-field', config['hudi_config']['source_ordering_field']
             ])
 
